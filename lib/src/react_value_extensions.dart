6cdee3e73eedc9089a5702ca2138d1c8499a61c9
Eduardo Pellenz <epellenz@tractian.com>
2023-09-28 22:24:53 -0300

    feat: create rxNull extensions

--- lib/src/react_value_extensions.dart
+++ lib/src/react_value_extensions.dart
@@ -1,52 +1,30 @@
 part of '../react_state.dart';
 
-// Primitive
+// Object
 
-extension ReactPrimInt on int {
-  @pragma("vm:prefer-inline")
-  ReactObject<int> get rx => ReactObject(this);
-}
-
-extension ReactPrimDouble on double {
-  @pragma("vm:prefer-inline")
-  ReactObject<double> get rx => ReactObject(this);
-}
-
-extension ReactPrimNum on num {
-  @pragma("vm:prefer-inline")
-  ReactObject<num> get rx => ReactObject(this);
-}
-
-extension ReactPrimBool on bool {
-  @pragma("vm:prefer-inline")
-  ReactObject<bool> get rx => ReactObject(this);
-}
-
-extension ReactPrimString on String {
-  @pragma("vm:prefer-inline")
-  ReactObject<String> get rx => ReactObject(this);
-}
-
-extension ReactPrimEnum<T extends Enum> on T {
+extension ReactObjectExtension<T> on T {
   @pragma("vm:prefer-inline")
   ReactObject<T> get rx => ReactObject(this);
+
+  @pragma("vm:prefer-inline")
+  ReactObject<T?> get rxNull => ReactObject(this);
 }
 
-extension ReactPrimObject<T> on T {
+extension ReactObjectNullExtension on Null {
   @pragma("vm:prefer-inline")
-  ReactObject<T> get rx => ReactObject(this);
+  ReactObject<T?> rxNull<T>() => ReactObject(this);
 }
 
 // List
 
-extension ReactPrimList<T> on List<T> {
+extension ReactListExtension<T> on List<T> {
   @pragma("vm:prefer-inline")
   ReactList<T> get rx => ReactList<T>(this);
 }
 
 // Map
 
-extension ReactPrimMap<K, V> on Map<K, V> {
+extension ReactMapExtension<K, V> on Map<K, V> {
   @pragma("vm:prefer-inline")
   ReactMap<K, V> get rx => ReactMap<K, V>(this);
 }
