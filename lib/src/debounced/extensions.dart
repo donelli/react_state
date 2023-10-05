f075971cb02f0de0b40f9c419e06901a95cee13c
Eduardo Pellenz <epellenz@tractian.com>
2023-10-04 22:04:46 -0300

    refactor: improve rxDebounced props

--- lib/src/debounced/extensions.dart
+++ lib/src/debounced/extensions.dart
@@ -1,7 +1,16 @@
 part of '../../react_state.dart';
 
 extension ReactValueDebouncedExtension<T> on T {
-  ReactValueDebounced<T> rxDebounced(Duration duration) {
-    return ReactValueDebounced<T>(this, duration);
+  ReactValueDebounced<T> rxDebounced({
+    int? milliseconds,
+    int? seconds,
+  }) {
+    return ReactValueDebounced<T>(
+      this,
+      Duration(
+        milliseconds: milliseconds ?? 0,
+        seconds: seconds ?? 0,
+      ),
+    );
   }
 }
