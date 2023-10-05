401381e7206220fe62dc1aeedfe3baa3620b25c6
Eduardo Pellenz <epellenz@tractian.com>
2023-10-04 22:05:07 -0300

    feat: allow to listen for changes without value

--- lib/src/react_state_change_notifier.dart
+++ lib/src/react_state_change_notifier.dart
@@ -3,17 +3,29 @@ part of '../react_state.dart';
 class ReactStateChangeNotifier<T> {
   void _notify(T value) {
     for (final callback in _callbacks.keys) {
-      callback(value);
+      if (callback is void Function(T)) {
+        callback(value);
+      } else {
+        callback();
+      }
     }
   }
 
-  final _callbacks = <void Function(T), void>{};
+  final _callbacks = <Function, void>{};
 
   void addListener(void Function(T) callback) {
     _callbacks[callback] = null;
   }
 
   void removeListener(void Function(T) callback) {
+    _callbacks.remove(callback);
+  }
+
+  void addListenerValueless(void Function() callback) {
+    _callbacks[callback] = null;
+  }
+
+  void removeListenerValueless(void Function() callback) {
     _callbacks.remove(callback);
   }
 }
